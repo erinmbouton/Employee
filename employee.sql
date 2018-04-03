daba3f371a5ddf5c689647049386f0d92fa25585
erinmbouton <erinmbouton@gmail.com>
2018-04-02 22:17:09 -0400

    Add files via upload

---
+++ employee.sql
@@ -0,0 +1,17 @@
+
+CREATE DATABASE employee;
+
+CREATE Table employeeid (
+  Date_of_Birth DATE,
+  Social_Security_Number TEXT,
+  Hire_Date DATE,
+  Gender TEXT,
+  Phone_Number TEXT,
+  Email_Address TEXT,
+  Street_Address TEXT,
+  Zipcode VARCHAR(5),
+  City TEXT,
+  State TEXT,
+  Country TEXT,
+  Hourly_Rate TEXT,
+);
