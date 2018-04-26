e3ea87cc1b137008f0e1be2ca2f38468fc0eb9d4
erinmbouton <erinmbouton@gmail.com>
2018-04-26 18:29:15 -0400

    Add files via upload

--- employee.sql
+++ employee.sql
@@ -1,7 +1,7 @@
-
 CREATE DATABASE employee;
 
 CREATE Table employeeid (
+  Employee_ID int,
   Date_of_Birth DATE,
   Social_Security_Number TEXT,
   Hire_Date DATE,
@@ -11,7 +11,17 @@ CREATE Table employeeid (
   Street_Address TEXT,
   Zipcode VARCHAR(5),
   City TEXT,
-  State TEXT,
+  State VARCHAR(5),
   Country TEXT,
+  Annual_Salary TEXT,
   Hourly_Rate TEXT,
-);
+  Weekly_Rate TEXT,
+  Quarterly_Rate TEXT,
+  From_Date DATE,
+  To_Date DATE,
+  Overtime TEXT,
+  Department_Number int,
+  Department_Name TEXT,
+  Employee_Title TEXT);
+  
+  
