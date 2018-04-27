f373a6d1010483748916e2827f3fd67d3c55d0c1
erinmbouton <erinmbouton@gmail.com>
2018-04-27 09:55:15 -0400

    Add files via upload

---
+++ employee.js
@@ -0,0 +1,7 @@
+function new_row() {
+       var tr = document.createElement("tr");
+       var td = document.createElement("td");
+       var txt = document.createElement("INPUT")
+       txt.setAttribute("type","text")
+       tr.appendChild(th);
+}
